97346d96dd30a40b78969635a55ac5046d0f3781
david660 <daodu.muyiwa@gmail.com>
2024-03-14 02:07:17 +0000

    Update Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'polysans-regular': ["PolySans Neutral"],
+        'polysans-slim': ["PolySans Slim"],
+        'clash-medium': ["ClashGrotesk-Medium"],
+        'polysans-semibold': ["PolySans Median"],
+        'polysans-bold': ["PolySans Bulky"],
+        'inter':["Inter"],
+        'geist':["GeistVariable"]
+      },
+    },
   },
   plugins: [],
 }
