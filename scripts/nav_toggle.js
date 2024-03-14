e8588f662925be4fded1409a9be1f482fa9084a6
david660 <daodu.muyiwa@gmail.com>
2024-03-14 06:39:04 +0000

    fit cards

--- scripts/nav_toggle.js
+++ scripts/nav_toggle.js
@@ -4,12 +4,10 @@ let line2 = document.querySelector(".line2")
 let line3 = document.querySelector(".line3")
 let navLinks = document.querySelector(".nav-links")
 
-console.log("yes")
-
-burger.addEventListener("click", ()=>{
+const toggleNav = () =>{
     line1.classList.toggle("animate-line1")
     line2.classList.toggle("hidden")
     line3.classList.toggle("animate-line3")
 
     navLinks.classList.toggle("hidden")
-})
+}
